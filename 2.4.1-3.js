e41a9450e0d58136e69d88619c8047d8429e6bf6
soy22 <jsy03070@naver.com>
2022-10-14 01:29:33 +0900

    src/App.js(Fragment사용)

---
+++ 2.4.1-3.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+ 
+function App() {
+  return (
+    <Fragment>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </Fragment>
+  );
+}
+ 
+export default App;
