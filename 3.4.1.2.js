a9365c311f5e2082116afc417807b0e5567ff06f
soy22 <jsy03070@naver.com>
2022-10-14 14:26:56 +0900

    Counter.js(state를 constructor에서 꺼내기)

---
+++ 3.4.1.2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+class Counter extends Component {
+  state = {
+    number: 0,
+    fixedNumber: 0
+  };
+  render() {
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+    return (...);
+  }
+}
+ 
+export default Counter;
